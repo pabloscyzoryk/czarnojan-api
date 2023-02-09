b4c40f47fe9ff05f044819fa8a787b7bd1dae6c7
Paweł Cyrzyk <95565770+pabloscyzoryk@users.noreply.github.com>
2023-02-09 21:10:48 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const sharp = require('sharp');
 const app = express();
+const fs = require('fs');
 
 app.use(express.json());
 
+app.get('/api/:dimensions', (req, res) => {
+    const dimensions = req.params.dimensions.split('x');
+    const width = parseInt(dimensions[0], 10);
+    const height = parseInt(dimensions[1], 10);
+
+    fs.readFile('./assets/czarnojan.png', async (data, err) => {
+        if (err) {
+            res.send('error :(');
+        }
+
+        else {
+            const resizedImage = await sharp(data)
+                .resize(width, height)
+                .toBuffer();
+
+            res.set('Content-Type', 'image/jpeg');
+            res.send(resizedImage);
+        }
+    })
+})
+
 app.get('/', (req, res) => {
-    res.send('<p>Czarnojan API</p>')
+    res.send('<p>Czarnojan API</p>');
 })
 
 const port = process.env.PORT || 3000;

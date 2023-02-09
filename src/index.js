282e8836dbd35400222c6887687faf234bed81c8
Paweł Cyrzyk <95565770+pabloscyzoryk@users.noreply.github.com>
2023-02-09 20:57:39 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('<p>Czarnojan API</p>')
 })
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Czarnojan listening on port ${port}`);
+});
